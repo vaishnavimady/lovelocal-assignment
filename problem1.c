5dced7a8465e2d3eead3e7176a703166f211e8c4
vaishnavimady <96937133+vaishnavimady@users.noreply.github.com>
2023-12-04 16:49:37 +0530

    problem1.c

--- problem1.c
+++ problem1.c
@@ -2,24 +2,21 @@
 #include <string.h>
 
 int lengthOfLastWord(char *s) {
-    int length = 0;
-    int i = strlen(s) - 1;
+    int length = 0;  // Variable to store the length of the last word
+    int i = strlen(s) - 1; //Starting from the last character of the string
 
-    while (i >= 0 && s[i] == ' ') {
-        i--;
-    }
-
+ // Calculating the length of the last word by counting characters until a space or the start of the string
     while (i >= 0 && s[i] != ' ') {
         length++;
         i--;
     }
 
-    return length;
+    return length;  //Returning the length of the last word
 }
 
 int main() {
-    char input[] = "Hello World";
-    int result = lengthOfLastWord(input);
-    printf("Length of last word: %d\n", result);
-    return 0;
+    char input[] = "Hello World"; // Input string
+    int result = lengthOfLastWord(input); // Calling the function to find the length of the last word
+    printf("Length of last word: %d\n", result); // Printing the length of the last word
+    return 0; // Return 0 to indicate successful execution
 }
